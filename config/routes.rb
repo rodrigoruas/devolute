d037500402801bf654df92b085f09ba57e6d8aa8
Rodrigo Ruas <rodrigoruas2@gmail.com>
2020-03-17 10:09:08 +0000

    create users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+
+  devise_for :users
+  root to: "pages#home"
+  resources :uploads, only: [:create, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
